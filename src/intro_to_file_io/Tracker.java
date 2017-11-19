6b9224019d4adc510dbe587ae27b6b92c7c04f1e
camman00 <league@WTS-iMac-11.attlocal.net>
2017-11-19 12:52:51 -0800

    Legitment sources
    The FDIPLOM approves the message!

--- src/intro_to_file_io/Tracker.java
+++ src/intro_to_file_io/Tracker.java
@@ -37,7 +37,7 @@ public class Tracker implements ActionListener {
 		jButton1 = new JButton();jButton2 = new JButton();jButton3 = new JButton();jButton4 = new JButton();
 		jPanel.add(jButton1);jPanel.add(jButton2);jPanel.add(jButton3);jPanel.add(jButton4);
 		jButton1.setText("Add Task");jButton2.setText("Remove Task");jButton3.setText("Save");jButton4.setText("Load");
-		//jButton1.addActionListener(this);jButton2.addActionListener(this);jButton3.addActionListener(this);jButton4.addActionListener(this);
+		jButton1.addActionListener(this);jButton2.addActionListener(this);jButton3.addActionListener(this);jButton4.addActionListener(this);
 		jFrame.setVisible(true);
 		jFrame.pack();
 	}
@@ -47,19 +47,20 @@ public class Tracker implements ActionListener {
 			String task = JOptionPane.showInputDialog("Add a task!");
 			toSave.add(task);
 		}
-		if(e.getSource() == jButton3) {
+		else if(e.getSource() == jButton3) {
 			for(String s : toSave) {
 				FileWriter fw;
 				try {
 					fw = new FileWriter("src/intro_to_file_io/data.txt",true);
 					fw.write(s);
+					fw.close();
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
 				finally {
 					try {
 						fw = new FileWriter("src/intro_to_file_io/data.txt",true);
-						fw.write("##");
+						fw.write("☕");
 						fw.close();
 					} catch (IOException e1) {
 						e1.printStackTrace();
@@ -67,5 +68,82 @@ public class Tracker implements ActionListener {
 				}
 			}
 		}
+		else if(e.getSource() == jButton2) {
+			int mint = Integer.valueOf(JOptionPane.showInputDialog("Remove element 1 of " + getCount()));
+			int count = mint;
+			int currentCount = 0;
+			int totalCount = 0;
+			while(totalCount != getTotalCount()) {
+				try {
+					FileReader fr = new FileReader("src/intro_to_file_io/data.txt");
+					int c = fr.read();
+					System.out.println((char)c);
+					while(c != -1){
+						if((char)c == '☕') {
+							currentCount++;
+						}
+						totalCount++;
+						if(count != currentCount) {
+							try {
+								FileWriter fw = new FileWriter("src/intro_to_file_io/test2.txt");
+								
+								fw.write(c);
+								
+								fw.write("\nThis is me writing a message");
+									
+								fw.close();
+							} catch (IOException e1) {
+								e1.printStackTrace();
+							}
+						}
+						c = fr.read();
+						
+					}
+					fr.close();
+				} catch (FileNotFoundException e1) {
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+	}
+	private int getCount() {
+		int count = 0;
+		try {
+			FileReader fr = new FileReader("src/intro_to_file_io/data.txt");
+			int c = fr.read();
+			while(c != -1){
+				if((char)c == '☕') {
+					count++;
+				}
+				c = fr.read();
+				
+			}
+			fr.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return count;
+	}
+	private int getTotalCount() {
+		int count = 0;
+		try {
+			FileReader fr = new FileReader("src/intro_to_file_io/data.txt");
+			int c = fr.read();
+			while(c != -1){
+				count++;
+				c = fr.read();
+				
+			}
+			fr.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return count;
 	}
 }
